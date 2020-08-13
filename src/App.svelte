dab7dc81eabb99a57328d39fc36f31a7c3e1768c
plloyd11 <plloyd11@gmail.com>
2020-08-12 22:47:07 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Header from './ui/Header.svelte'
+    let count = 0
+    let addShit = () => {
+        count++
+    }
+    $: console.log(`${count}`)
+    let list = [1,2,3,4]
+</script>
+
+<main>
+    <Header />
+    <p>{count}</p>
+    <button on:click={addShit}>Add Shit!</button>
+    <ul class="flex">
+        {#each list as item}
+            <li>{item}</li>
+        {/each}
+    </ul>
+</main>
